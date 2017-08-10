cdec02551a0c93427d9f5a07828a6a3a33e1ec7a
Guido_Fe <guidoferris@gmail.com>
2017-08-11 00:13:33 +0200

    Neofetch and lock modified

--- .lock_script.sh
+++ .lock_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 import -window root /tmp/screen.jpg
 mogrify -blur 0x8 /tmp/screen.jpg
-icon="$HOME/.lock_icon.png"
-if [ -f $icon ]; then
-	composite -gravity Center $HOME/.lock_icon.png /tmp/screen.jpg /tmp/screen.png
-fi
+#icon="$HOME/.lock_icon.png"
+#if [ -f $icon ]; then
+#	composite -gravity Center $HOME/.lock_icon.png /tmp/screen.jpg /tmp/screen.png
+#fi
+convert /tmp/screen.jpg -font Rope-MF -pointsize 200 -gravity Center -annotate +0+0 "Locked" /tmp/screen.png
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.*
